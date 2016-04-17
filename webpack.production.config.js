d95d1945ef201a8cb138650df1cae212aef8f8cf
trump27 <smilebowl-trump27@yahoo.co.jp>
2016-04-17 11:08:13 +0900

    use webpack.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,43 +1,25 @@
-var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var config = require('./webpack.config.js')
 
-module.exports = {
-  entry: [
-    './src/client/index.js'
-  ],
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    publicPath: '/',
-    filename: 'build.js'
-  },
-  module: {
-    preLoaders: [
-      { test: /\.vue$/, loader: 'eslint', exclude: /node_modules/ }
-    ],
-    loaders: [
-      { test: /\.vue$/, loader: 'vue' },
-      { test: /\.js$/, loader: 'babel!eslint', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json' },
-      { test: /\.html$/, loader: 'vue-html' }
-    ]
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body'
-    })
-  ]
-}
+config.entry.shift()
+config.devtool = 'source-map'
+config.plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': '"production"'
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    inject: 'body'
+  })
+]
+
+module.exports = config
